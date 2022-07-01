f53e3b1b4c9837d35fe4453d01245cd8d40aec54
DESKTOP-SB3UMDG\Shashank-PC <shashank2361@yahoo.co.in>
2022-07-01 01:32:01 +0100

    Add project files.

---
+++ Movies.Api/Data/MoviesApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Movies.Api.Model;
+
+namespace Movies.Api.Data
+{
+    public class MoviesApiContext : DbContext
+    {
+        public MoviesApiContext (DbContextOptions<MoviesApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movies.Api.Model.Movie> Movie { get; set; }
+    }
+}
---
+++ Movies.Api/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Movies.Api.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            SeedDatabase(host);
+            host.Run();
+         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        private static void SeedDatabase(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var moviesContext = services.GetRequiredService<MoviesApiContext>();
+                MoviesContextSeed.SeedAsync(moviesContext);
+            }
+        }
+
+    }
+}
